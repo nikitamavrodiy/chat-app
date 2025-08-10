a87593bdedc5fb2532c464458aa216610c95f2f0
nikitamavrodiy <106612451+nikitamavrodiy@users.noreply.github.com>
2025-08-10 22:46:47 +0300

    socket()'s error handling updated

--- src/server.cpp
+++ src/server.cpp
@@ -6,19 +6,19 @@
 
 int main()
 {
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0); // Create a socket
 
-    if (server_fd < 0)
-    { 
-        std::cerr << "Socket creation failed\n";
+    if (server_fd == -1)
+    {
+        printf("errno = %d (%s)\n", errno, strerror(errno));
         return 1;
     }
-
     std::cout << "Socket created! FD = " << server_fd << "\n";
 
+    
     sockaddr_in address{}; // Initialize the address structure
     address.sin_family = AF_INET; // IPv4
-    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address
+    address.sin_addr.s_addr = INADDR_ANY; // Accept connections from any IP address. 
     address.sin_port = htons(8080); // Port 8080
 
     if (bind(server_fd, reinterpret_cast<struct sockaddr*>(&address), sizeof(address)) < 0)
